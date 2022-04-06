22c6b27504f0d6b97460e7cea13bd8fcc3827b7f
YogeshSuryawanhi-17 <suryawanshiyogesh1998@yahoo.com>
2022-04-06 21:00:13 +0530

    logger pushed

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -3,4 +3,4 @@ let log =  function welcome()
     console.log("Welcome to my application. I am Yogesh  and a part of FunctionUp Thorium cohort.")
 }
 
-module.exports.call = lo
+module.exports.call = log
