58db5d3bad62e897569dae35433018322661421c
YogeshSuryawanhi-17 <suryawanshiyogesh1998@yahoo.com>
2022-04-06 20:22:33 +0530

    logger pushed

---
+++ src/logger/logger.js
@@ -0,0 +1,6 @@
+let log =  function welcome()
+{
+    console.log("Welcome to my application. I am Yogesh  and a part of FunctionUp Thorium cohort.")
+}
+
+module.exports.call = log
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,19 @@
 const express = require('express');
-
+const logger =  require('../logger/logger');
 const router = express.Router();
 
+
 router.get('/test-me', function (req, res) {
+  
+  
+   // console.log("insie first")
+    //console.log("calling logging function")
     res.send('My first ever api!')
+    logger.call();
+    
+});
+router.get('/test-me1', function (req, res) {git 
+    res.send('My first ever api 2!')
 });
 
 module.exports = router;
