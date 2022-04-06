7bca62e356a341f8d3be23235a2f4fd8c8b6faaf
YogeshSuryawanhi-17 <suryawanshiyogesh1998@yahoo.com>
2022-04-06 21:55:53 +0530

    validater added

---
+++ src/validator/formatter.js
@@ -0,0 +1,12 @@
+
+let format = function formatter()
+
+{
+const strformat ="  FunctionUp  "
+
+console.log("stringTrim O/P :",strformat.trim())
+console.log("stringTolower: ",strformat.toLowerCase());
+console.log("stringTolower: ",strformat.toUpperCase());
+
+}
+module.exports.formatter = format
