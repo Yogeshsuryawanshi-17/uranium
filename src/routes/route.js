9875f00175f0c5cb8c3dc2cc778046cd6084fc6d
YogeshSuryawanhi-17 <suryawanshiyogesh1998@yahoo.com>
2022-04-07 21:54:35 +0530

    session/get-path-params

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,12 +3,37 @@ const logger = require('./logger')
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
+router.get('/t', function (req, res) {
+    // console.log('------------------')
+    // console.log(req)
+    // console.log('------------------')
+    // console.log('These are the request query parameters: ', req.query)
+     //res.send('My first ever api!')
+
+   const arr1 =[
+          'Yogesh',
+          'managesh',
+          'ganesh',
+          'pratik',
+          'anil',
+          'richcard',
+          'rita',
+          'shrdhha',
+          'sunil',
+          'shubhangi'
+   ]
+
+  //res.send(arr1)
+ 
+
+
+
+  res.send(req.query)
+
+  res.send(arr1.length=req.query)
+   
+     
+
 });
 
 
