d5af9da07d2b20bd403149fc5dbf1e97b5a36f31
YogeshSuryawanhi-17 <suryawanshiyogesh1998@yahoo.com>
2022-04-07 23:05:17 +0530

    session/get-query-params

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,38 +1,53 @@
+const { query } = require('express');
 const express = require('express');
 const logger = require('./logger')
 
 const router = express.Router();
 
-router.get('/t', function (req, res) {
+const arr1 =[
+  'Yogesh',
+  'managesh',
+  'ganesh',
+  'pratik',
+  'anil',
+  'richcard',
+  'rita',
+  'shrdhha',
+  'sunil',
+  'shubhangi'
+]
+
+router.get('/all-candidates', function (req, res) {
     // console.log('------------------')
     // console.log(req)
     // console.log('------------------')
     // console.log('These are the request query parameters: ', req.query)
      //res.send('My first ever api!')
-
-   const arr1 =[
-          'Yogesh',
-          'managesh',
-          'ganesh',
-          'pratik',
-          'anil',
-          'richcard',
-          'rita',
-          'shrdhha',
-          'sunil',
-          'shubhangi'
-   ]
-
-  //res.send(arr1)
- 
-
-
-
-  res.send(req.query)
-
-  res.send(arr1.length=req.query)
    
-     
+
+   
+  res.send(arr1)   
+});
+router.get('/candidates', function (req, res){
+  
+         
+
+ const cntarr = req.query.count;
+ const arr = [];
+   for(let i=0;i<arr1.length;i++)
+   {
+
+     if(cntarr>i)
+      {
+
+         arr[i] = arr1[i]
+          
+      }
+         
+   }
+   
+res.send(arr)
+
 
 });
 
