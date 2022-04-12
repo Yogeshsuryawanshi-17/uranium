f1363a9d512b395d6fa77be54815f15b4b607e03
YogeshSuryawanhi-17 <suryawanshiyogesh1998@yahoo.com>
2022-04-12 21:01:54 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
-const { default: mongoose } = require('mongoose');
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Yogesh_Mjs:<password>@cluster0.8e1bd.mongodb.net/Yogesh_Mjs_DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
