ab0e34d41672bf1b7a7ee2fd9eddb0fd9c5bc3ab
YogeshSuryawanhi-17 <suryawanshiyogesh1998@yahoo.com>
2022-04-15 21:34:42 +0530

    DataPush

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const UserModel = require("../models/userModel.js")
-const UserController = require("../controller/usercontroller.js")
-const BookModel = require("../models/Bookmodel.js")
+const BookModel = require("../models/Bookmodel")
 const Bookcontroller = require("../controller/BookController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post('/createuser', UserController.createuserData)
-router.get('/getuserdata', UserController.findUser)
+
 
 router.post('/createBook',Bookcontroller.createBook)
 router.get('/getBooklist',Bookcontroller.FindBook)
